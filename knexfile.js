247f9851d2241346a0bbc5ec68f73c7d5a21bcac
PascalUlor <pascalulor@yahoo.com>
2019-07-25 12:37:16 +0100

    initial commit set up environment and database

---
+++ knexfile.js
@@ -0,0 +1,30 @@
+// Update with your config settings.
+
+module.exports = {
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/userdb.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seed: {
+      directory: './database/seeds'
+    },
+    useNullAsDefault: true
+  },
+  test: {
+    client: 'sqlite3',
+    connection: {
+      filename: './database/test.db3'
+    },
+    migrations: {
+      directory: './database/migrations'
+    },
+    seed: {
+      directory: './database/seeds'
+    },
+    useNullAsDefault: true
+  }
+};
